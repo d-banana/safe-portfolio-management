1082cb946a2291d9482f015f9105b2f396c44e58
D-Banana <d-banana@mail.com>
2023-04-20 17:15:49 +0200

    Start implementing a new version of market simulation using simple model with 4 actors(taker buyer/seller and maker buyer/seller). Need refactor.

--- simulation/src/lib.rs
+++ simulation/src/lib.rs
@@ -3,12 +3,17 @@ use crate::runner::Runner;
 use chrono::{DateTime, Duration, TimeZone, Utc};
 pub use core::*;
 
-mod maker;
+mod market_state;
 mod runner;
-mod taker;
 
 pub fn generate_price_graph() -> Vec<(DateTime<Utc>, f64)> {
-    let mut runner = Runner::new(200 * 24 * 60 * 60 * 1000, 0.001, 1_900.0).unwrap();
+    let mut runner = Runner::new(
+        200 * 24 * 60 * 60 * 1000,
+        0.1,
+        1_900.0,
+        (15, 15_000),
+        (14 * 24 * 60 * 60 * 1000, 60 * 24 * 60 * 60 * 1000),
+    );
 
     let ticks = runner.run().unwrap();
     let hlocs = Hloc::from_tick_vec(ticks, 4 * 60 * 60 * 1000).unwrap();
--- simulation/src/runner.rs
+++ simulation/src/runner.rs
@@ -1,40 +1,38 @@
-use crate::maker::{Maker, MakerError, ProvideLiquidity};
 use crate::market::{Tick, TickError};
-use crate::taker::{TakeLiquidity, Taker, TakerError};
-use chrono::{DateTime, Duration, Utc};
+use crate::market_state::*;
 use rand::{thread_rng, Rng};
-use thiserror::Error;
+use std::io::Error;
 
-// RUNNER
-const GRANULARITY_EPOCH_MILLISECOND: (u64, u64) = (5, 30_000);
-const START_DATE: DateTime<Utc> = DateTime::<Utc>::MIN_UTC;
-const MOVING_AVERAGE_DURATION_MILLISECOND: u64 = 24 * 60 * 60 * 100;
-const LIQUIDITY_MULTIPLIER: (f64, f64) = (1.0, 4.0);
-
-// TAKER
-const FEAR_START: f64 = 0.5;
-const GREED_START: f64 = 0.5;
-
-#[derive(Error, Debug, PartialEq)]
-pub enum RunnerError {
-    #[error("Price increment should be between 0.0 and 1.0")]
-    PriceIncrementIsPercentDecimal,
-    #[error("Price should always be positive...")]
-    PriceShouldPositiveError,
-    #[error("Something went wrong with Maker(`{0}`)")]
-    MakerError(MakerError),
-    #[error("Something went wrong with Taker(`{0}`)")]
-    TakerError(TakerError),
-    #[error("Something went wrong with Tick(`{0}`)")]
-    TickError(TickError),
-    #[error("Unexpected error...")]
-    UnexpectedError,
+pub struct ProvideLiquidity {
+    pub spread_tick: usize,
+    pub liquidity_by_tick: f64,
+    pub liquidity_rate_of_change_by_tick: f64,
 }
 
+impl ProvideLiquidity {
+    pub fn new(
+        spread_tick: usize,
+        liquidity_by_tick: f64,
+        liquidity_rate_of_change_by_tick: f64,
+    ) -> Self {
+        Self {
+            spread_tick,
+            liquidity_by_tick,
+            liquidity_rate_of_change_by_tick,
+        }
+    }
+}
+
+const LIQUIDITY_BEST: (f64, f64) = (0.01, 1.0);
+const LIQUIDITY_CHANGE_BY_TICK: (f64, f64) = (0.01, 1.0);
+const ACTOR_LIQUIDITY_AMPLIFIER: f64 = 1.005;
+const LIMIT_LIQUIDITY_AMPLIFIER: f64 = 1.0;
 pub struct Runner {
     pub duration_millisecond: u64,
     pub price_increment_percent: f64,
     pub start_price: f64,
+    pub range_duration_between_trade_millisecond: (u64, u64),
+    pub range_duration_between_state_millisecond: (u64, u64),
 }
 
 impl Runner {
@@ -42,121 +40,212 @@ impl Runner {
         duration_millisecond: u64,
         price_increment_percent: f64,
         start_price: f64,
-    ) -> Result<Self, RunnerError> {
-        if start_price.is_sign_negative() {
-            return Err(RunnerError::PriceShouldPositiveError);
-        }
-        let is_price_increment_pct_decimal =
-            price_increment_percent >= 0.0 && price_increment_percent <= 1.0;
-        if !is_price_increment_pct_decimal {
-            return Err(RunnerError::PriceIncrementIsPercentDecimal);
-        }
-
-        Ok(Self {
+        range_duration_between_trade_millisecond: (u64, u64),
+        range_duration_between_state_millisecond: (u64, u64),
+    ) -> Self {
+        Self {
             duration_millisecond,
             price_increment_percent,
             start_price,
-        })
+            range_duration_between_trade_millisecond,
+            range_duration_between_state_millisecond,
+        }
     }
 
-    pub fn run(&self) -> Result<Vec<Tick>, RunnerError> {
+    pub fn run(&self) -> Result<Vec<Tick>, Error> {
         let mut rng = thread_rng();
         let mut ticks: Vec<Tick> = Vec::new();
-        let mut maker: Maker = self.init_maker()?;
-        let mut taker: Taker = self.init_taker()?;
-        let mut last_price = self.start_price;
-        let mut moving_average: Option<f64> = None;
-
         let mut current_time_millisecond: u64 = 0;
+        let mut current_price = self.start_price;
+
         while current_time_millisecond < self.duration_millisecond {
-            let (liquidity_ask, liquidity_bid) = maker
-                .provide_liquidity()
-                .map_err(|e| RunnerError::MakerError(e))?;
-            let take_liquidity = taker
-                .take_liquidity()
-                .map_err(|e| RunnerError::TakerError(e))?;
-
-            if take_liquidity.is_buy {
-                ticks.append(&mut self.match_order(
-                    last_price,
-                    current_time_millisecond,
-                    liquidity_ask,
-                    take_liquidity,
-                )?);
+            let mut market_state = MARKET_STATE::MB_GREATER_LS_MS_LESS_LB;
+            let mut duration_market_state_millisecond = rng.gen_range(
+                self.range_duration_between_state_millisecond.0
+                    ..=self.range_duration_between_state_millisecond.1,
+            );
+            duration_market_state_millisecond = if duration_market_state_millisecond
+                + current_time_millisecond
+                > self.duration_millisecond
+            {
+                self.duration_millisecond - current_time_millisecond
             } else {
-                ticks.append(&mut self.match_order(
-                    last_price,
-                    current_time_millisecond,
-                    liquidity_bid,
-                    take_liquidity,
-                )?);
-            }
-
+                duration_market_state_millisecond
+            };
+            ticks.append(&mut self.process_market_state(
+                &current_price,
+                &market_state,
+                &mut current_time_millisecond,
+                &duration_market_state_millisecond,
+            ));
+            current_time_millisecond += duration_market_state_millisecond;
             if ticks.last().is_some() {
-                last_price = ticks.last().ok_or(RunnerError::UnexpectedError)?.price;
+                current_price = ticks.last().unwrap().price;
             }
-            current_time_millisecond +=
-                rng.gen_range(GRANULARITY_EPOCH_MILLISECOND.0..GRANULARITY_EPOCH_MILLISECOND.1);
         }
 
         Ok(ticks)
     }
 
-    fn init_maker(&self) -> Result<Maker, RunnerError> {
-        let maker = Maker::new(LIQUIDITY_MULTIPLIER.0);
-        maker.map_err(|e| RunnerError::MakerError(e))
-    }
-
-    fn init_taker(&self) -> Result<Taker, RunnerError> {
-        let taker = Taker::new(LIQUIDITY_MULTIPLIER.0, FEAR_START, GREED_START);
-        taker.map_err(|e| RunnerError::TakerError(e))
-    }
-
-    fn match_order(
+    pub fn process_market_state(
         &self,
-        last_price: f64,
-        current_time_millisecond: u64,
-        provide_liquidity: ProvideLiquidity,
-        take_liquidity: TakeLiquidity,
-    ) -> Result<Vec<Tick>, RunnerError> {
+        current_price: &f64,
+        market_state: &MARKET_STATE,
+        current_time_millisecond: &mut u64,
+        duration_market_state_millisecond: &u64,
+    ) -> Vec<Tick> {
         let mut rng = thread_rng();
         let mut ticks: Vec<Tick> = Vec::new();
-        let mut volume_left = take_liquidity.quantity;
-        let mut liquidity_tick = provide_liquidity.liquidity_by_tick;
-        let mut price = if take_liquidity.is_buy {
-            last_price
-                + (last_price * self.price_increment_percent * provide_liquidity.spread_tick as f64)
-        } else {
-            last_price
-                - (last_price * self.price_increment_percent * provide_liquidity.spread_tick as f64)
-        };
-        let mut current_time_millisecond = current_time_millisecond as i64;
+        let mut duration_market_state_millisecond = duration_market_state_millisecond.clone();
+        let mut current_time_millisecond = current_time_millisecond.clone();
+        let mut current_price = current_price.clone();
+        let actor_power = market_state.actor_power();
 
-        while volume_left > 0.0 && liquidity_tick > 0.0 {
-            let volume = if volume_left < liquidity_tick {
-                volume_left
-            } else {
-                liquidity_tick
+        while duration_market_state_millisecond > 0 {
+            let mut limit_sell_volume_by_tick = match actor_power.market_buyer_vs_limit_seller {
+                ACTOR_POWER_STATE::LESS => {
+                    rng.gen_range(LIQUIDITY_BEST.0..=LIQUIDITY_BEST.1) * ACTOR_LIQUIDITY_AMPLIFIER
+                }
+                ACTOR_POWER_STATE::EQUAL => rng.gen_range(LIQUIDITY_BEST.0..=LIQUIDITY_BEST.1),
+                ACTOR_POWER_STATE::GREATER => {
+                    rng.gen_range(LIQUIDITY_BEST.0..=LIQUIDITY_BEST.1) / ACTOR_LIQUIDITY_AMPLIFIER
+                }
+            } * LIMIT_LIQUIDITY_AMPLIFIER;
+            let limit_sell_volume_change_by_tick = match actor_power.market_buyer_vs_limit_seller {
+                ACTOR_POWER_STATE::LESS => {
+                    rng.gen_range(LIQUIDITY_CHANGE_BY_TICK.0..=LIQUIDITY_CHANGE_BY_TICK.1)
+                        * ACTOR_LIQUIDITY_AMPLIFIER
+                }
+                ACTOR_POWER_STATE::EQUAL => {
+                    rng.gen_range(LIQUIDITY_CHANGE_BY_TICK.0..=LIQUIDITY_CHANGE_BY_TICK.1)
+                }
+                ACTOR_POWER_STATE::GREATER => {
+                    rng.gen_range(LIQUIDITY_CHANGE_BY_TICK.0..=LIQUIDITY_CHANGE_BY_TICK.1)
+                        / ACTOR_LIQUIDITY_AMPLIFIER
+                }
+            } * LIMIT_LIQUIDITY_AMPLIFIER;
+
+            let mut limit_buy_volume_by_tick = match actor_power.market_seller_vs_limit_buyer {
+                ACTOR_POWER_STATE::LESS => {
+                    rng.gen_range(LIQUIDITY_BEST.0..=LIQUIDITY_BEST.1) * ACTOR_LIQUIDITY_AMPLIFIER
+                }
+                ACTOR_POWER_STATE::EQUAL => rng.gen_range(LIQUIDITY_BEST.0..=LIQUIDITY_BEST.1),
+                ACTOR_POWER_STATE::GREATER => {
+                    rng.gen_range(LIQUIDITY_BEST.0..=LIQUIDITY_BEST.1) / ACTOR_LIQUIDITY_AMPLIFIER
+                }
+            } * LIMIT_LIQUIDITY_AMPLIFIER;
+            let limit_buy_volume_change_by_tick = match actor_power.market_seller_vs_limit_buyer {
+                ACTOR_POWER_STATE::LESS => {
+                    rng.gen_range(LIQUIDITY_CHANGE_BY_TICK.0..=LIQUIDITY_CHANGE_BY_TICK.1)
+                        * ACTOR_LIQUIDITY_AMPLIFIER
+                }
+                ACTOR_POWER_STATE::EQUAL => {
+                    rng.gen_range(LIQUIDITY_CHANGE_BY_TICK.0..=LIQUIDITY_CHANGE_BY_TICK.1)
+                }
+                ACTOR_POWER_STATE::GREATER => {
+                    rng.gen_range(LIQUIDITY_CHANGE_BY_TICK.0..=LIQUIDITY_CHANGE_BY_TICK.1)
+                        / ACTOR_LIQUIDITY_AMPLIFIER
+                }
+            } * LIMIT_LIQUIDITY_AMPLIFIER;
+
+            let mut market_buy_volume = match actor_power.market_buyer_vs_limit_seller {
+                ACTOR_POWER_STATE::GREATER => {
+                    rng.gen_range(LIQUIDITY_BEST.0..=LIQUIDITY_BEST.1) * ACTOR_LIQUIDITY_AMPLIFIER
+                }
+                ACTOR_POWER_STATE::EQUAL => rng.gen_range(LIQUIDITY_BEST.0..=LIQUIDITY_BEST.1),
+                ACTOR_POWER_STATE::LESS => {
+                    rng.gen_range(LIQUIDITY_BEST.0..=LIQUIDITY_BEST.1) / ACTOR_LIQUIDITY_AMPLIFIER
+                }
             };
+            let mut market_sell_volume = match actor_power.market_seller_vs_limit_buyer {
+                ACTOR_POWER_STATE::GREATER => {
+                    rng.gen_range(LIQUIDITY_BEST.0..=LIQUIDITY_BEST.1) * ACTOR_LIQUIDITY_AMPLIFIER
+                }
+                ACTOR_POWER_STATE::EQUAL => rng.gen_range(LIQUIDITY_BEST.0..=LIQUIDITY_BEST.1),
+                ACTOR_POWER_STATE::LESS => {
+                    rng.gen_range(LIQUIDITY_BEST.0..=LIQUIDITY_BEST.1) / ACTOR_LIQUIDITY_AMPLIFIER
+                }
+            };
+
+            if rng.gen_bool(0.5) {
+                ticks.append(&mut self.process_market_order(
+                    &current_price,
+                    &current_time_millisecond,
+                    &market_buy_volume,
+                    &limit_sell_volume_by_tick,
+                    &limit_sell_volume_change_by_tick,
+                    &true,
+                ));
+            } else {
+                ticks.append(&mut self.process_market_order(
+                    &current_price,
+                    &current_time_millisecond,
+                    &market_sell_volume,
+                    &limit_buy_volume_by_tick,
+                    &limit_buy_volume_change_by_tick,
+                    &false,
+                ));
+            }
+            if ticks.last().is_some() {
+                current_price = ticks.last().unwrap().price;
+            }
+            let wait_millisecond = rng.gen_range(
+                self.range_duration_between_trade_millisecond.0
+                    ..=self.range_duration_between_trade_millisecond.1,
+            );
+            current_time_millisecond += wait_millisecond;
+            duration_market_state_millisecond -=
+                if wait_millisecond > duration_market_state_millisecond {
+                    duration_market_state_millisecond
+                } else {
+                    wait_millisecond
+                };
+        }
+
+        ticks
+    }
+
+    pub fn process_market_order(
+        &self,
+        current_price: &f64,
+        current_time_millisecond: &u64,
+        market_volume: &f64,
+        limit_volume: &f64,
+        limit_volume_change: &f64,
+        is_buy: &bool,
+    ) -> Vec<Tick> {
+        let mut ticks: Vec<Tick> = Vec::new();
+        let mut market_volume_left = market_volume.clone();
+        let mut limit_volume_left = limit_volume.clone();
+        let mut current_price = current_price.clone();
+
+        while market_volume_left > 0.0 {
+            let is_liquidity_consumed = market_volume_left > limit_volume_left;
+            let volume = if !is_liquidity_consumed {
+                market_volume_left
+            } else {
+                limit_volume_left
+            };
+            market_volume_left -= volume;
 
             ticks.push(
                 Tick::new(
-                    price,
-                    current_time_millisecond,
+                    current_price,
+                    *current_time_millisecond as i64,
                     volume,
-                    take_liquidity.is_buy,
+                    *is_buy,
                 )
-                .map_err(|e| RunnerError::TickError(e))?,
+                .unwrap(),
             );
-
-            volume_left -= volume;
-            liquidity_tick += provide_liquidity.liquidity_rate_of_change_by_tick;
-            price = if take_liquidity.is_buy {
-                price + (price * self.price_increment_percent)
-            } else {
-                price - (price * self.price_increment_percent)
-            };
+            if is_liquidity_consumed {
+                if *is_buy {
+                    current_price = current_price + self.price_increment_percent;
+                } else {
+                    current_price = current_price - self.price_increment_percent;
+                }
+                limit_volume_left += limit_volume_change;
+            }
         }
-        Ok(ticks)
+        ticks
     }
 }
